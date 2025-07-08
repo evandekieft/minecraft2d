45a273106ead3156e16a03ef660165abd8687a2a
Erik Vandekieft <evk@meta.com>
2025-07-08 12:15:34 -0700

    Reduce number of pixels per block so as to see more blocks

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@
 INVENTORY_HEIGHT = 120  # Height of inventory area at bottom (scaled up)
 GAME_HEIGHT = 1200  # Height of game area
 WINDOW_SIZE = (1600, GAME_HEIGHT + INVENTORY_HEIGHT)
-GRID_SIZE = 64  # Size of each grid cell in pixels
+GRID_SIZE = 32  # Size of each grid cell in pixels
 GRID_WIDTH = WINDOW_SIZE[0] // GRID_SIZE
 GRID_HEIGHT = GAME_HEIGHT // GRID_SIZE
 

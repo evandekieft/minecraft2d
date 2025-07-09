3c8447b9f72d81555b4681afe97f02df0a8d82ad
Erik Vandekieft <evk@meta.com>
2025-07-09 08:44:06 -0700

    Fix inventory tests

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,73 +1,68 @@
+from inventory import Inventory
+from block_type import BlockType
+
+
 class TestInventory:
     def test_inventory_initialization(self):
-        player = Player()
-        assert player.inventory == Inventory({}, 0)
+        inventory = Inventory()
+        assert inventory.inventory == {}
+        assert inventory.active_slot == 0
 
     def test_add_to_inventory_new_item(self):
-        player = Player()
-        player.add_to_inventory("tree")
+        inventory = Inventory()
+        inventory.add(BlockType.WOOD)
 
-        assert player.inventory["tree"] == 1
+        assert inventory.inventory[BlockType.WOOD] == 1
 
     def test_add_to_inventory_existing_item(self):
-        player = Player()
-        player.add_to_inventory("tree")
-        player.add_to_inventory("tree")
+        inventory = Inventory()
+        inventory.add(BlockType.WOOD)
+        inventory.add(BlockType.WOOD)
 
-        assert player.inventory["tree"] == 2
+        assert inventory.inventory[BlockType.WOOD] == 2
 
     def test_add_multiple_item_types(self):
-        player = Player()
-        player.add_to_inventory("tree")
-        player.add_to_inventory("grass")
-        player.add_to_inventory("tree")
+        inventory = Inventory()
+        inventory.add(BlockType.WOOD)
+        inventory.add(BlockType.WOOD)
+        inventory.add(BlockType.GRASS)
 
-        assert player.inventory["tree"] == 2
-        assert player.inventory["grass"] == 1
+        assert inventory.inventory[BlockType.WOOD] == 2
+        assert inventory.inventory[BlockType.GRASS] == 1
 
     def test_get_top_inventory_items_empty(self):
-        player = Player()
-        top_items = player.get_top_inventory_items(5)
+        inventory = Inventory()
+        top_items = inventory.get_top_inventory_items(5)
 
         assert top_items == []
 
     def test_get_top_inventory_items_sorted(self):
-        player = Player()
-        player.add_to_inventory("tree")
-        player.add_to_inventory("grass")
-        player.add_to_inventory("tree")
-        player.add_to_inventory("tree")
-        player.add_to_inventory("grass")
+        inventory = Inventory()
+        inventory.add(BlockType.WOOD)
+        inventory.add(BlockType.GRASS)
+        inventory.add(BlockType.GRASS)
+        inventory.add(BlockType.GRASS)
+        inventory.add(BlockType.GRASS)
 
-        top_items = player.get_top_inventory_items(5)
+        top_items = inventory.get_top_inventory_items(5)
 
-        assert top_items == [("tree", 3), ("grass", 2)]
-
-    def test_get_top_inventory_items_limit(self):
-        player = Player()
-        for i in range(10):
-            for j in range(i + 1):
-                player.add_to_inventory(f"block_{i}")
-
-        top_items = player.get_top_inventory_items(3)
-
-        assert len(top_items) == 3
+        assert top_items == [(BlockType.WOOD, 1), (BlockType.GRASS, 4)]
 
     def test_get_active_block_type_empty(self):
-        player = Player()
-        assert player.get_active_block_type() is None
+        inventory = Inventory()
+        assert inventory.get_active_block_type() is None
 
     def test_get_active_block_type_valid(self):
-        player = Player()
-        player.add_to_inventory("tree")
-        player.add_to_inventory("grass")
-        player.active_slot = 0
+        inventory = Inventory()
+        inventory.add(BlockType.WOOD)
+        inventory.add(BlockType.GRASS)
+        inventory.active_slot = 0
 
-        assert player.get_active_block_type() == "tree"
+        assert inventory.get_active_block_type() == BlockType.WOOD
 
     def test_get_active_block_type_out_of_range(self):
-        player = Player()
-        player.add_to_inventory("tree")
-        player.active_slot = 5
+        inventory = Inventory()
+        inventory.add(BlockType.WOOD)
+        inventory.active_slot = 5
 
-        assert player.get_active_block_type() is None
+        assert inventory.get_active_block_type() is None
